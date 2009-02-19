ce0cea098c2faaedc50a2c5aff48873e5eb1a978
grtvd <grtvd@users.noreply.github.com>
2009-02-19 17:10:15 -0500

    dls: Updated AppSettings location of AppDataPath to match local show path since gateway applet can't access restricted directories.

--- common/Common/AppSettings.cs
+++ common/Common/AppSettings.cs
@@ -9,13 +9,26 @@ namespace iNetVOD.Common
 {
 	public class AppSettings
 	{
+		#region Constants
+		private static readonly string AppDirName = "Storm Media Player";
+		#endregion
+
 		#region Fields
-		private static string fAppDataPath = Path.Combine(Environment.GetFolderPath(
-			Environment.SpecialFolder.CommonApplicationData), "Storm Media Player");
+		private static string fAppDataPath = DetermineLocalShowPath();
 		#endregion
 
 		#region Properties
 		public static string AppDataPath { get { return fAppDataPath; } }
+		#endregion
+
+		#region Implementation
+		private static string DetermineLocalShowPath()
+		{
+			string localShowPath = "C:\\Users\\Public\\Videos";	//first try for Vista
+			if(!(new DirectoryInfo(localShowPath)).Exists)
+				localShowPath = "C:\\Documents and Settings\\All Users\\Documents\\My Videos";	//then try for XP
+			return Path.Combine(localShowPath, AppDirName);
+		}
 		#endregion
 	}
 }
