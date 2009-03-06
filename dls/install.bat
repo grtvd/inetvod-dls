0ed8f5ddd006aeee502c04dbe26c824fcf4083e0
grtvd <grtvd@users.noreply.github.com>
2009-03-06 17:02:58 -0500

    dls: Added manual install/uninstall bat files for dls for testing.

---
+++ dls/install.bat
@@ -0,0 +1,3 @@
+C:\Windows\Microsoft.NET\Framework\v1.1.4322\InstallUtil /LogToConsole=false C:\projects\iNetVOD\dls\trunk\bin\Release\storm.dls.exe
+@rem C:\Windows\Microsoft.NET\Framework\v2.0.50727\InstallUtil 
+pause
